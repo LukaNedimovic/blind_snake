b141e181cb3f8aa6bef3485add5b931085ea8aa2
Luka Nedimović <lukazen123@gmail.com>
2024-12-30 23:06:59 +0100

    remove unnecessary files and minor typo fix

--- experiments/scripts/sweep__coiling__large.sh
+++ experiments/scripts/sweep__coiling__large.sh
@@ -7,5 +7,5 @@ $SRC_DIR/sweep_boards.py \
     --max_width 1000 \
     --max_height 1000 \
     --shots 1 \
-    --runs 1000 \
+    --runs 100 \
     --out_path "$EXPERIMENTS_DIR/runs/sweep__coiling__large.csv"
